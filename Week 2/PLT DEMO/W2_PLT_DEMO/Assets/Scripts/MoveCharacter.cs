76bc118a3321e5dd31c0077441d22503adc4daaa
Zabir Al Nazi Nabil <zabiralnazi@codeassign.com>
2017-04-25 04:27:04 +0600

    nu

---
+++ Week 2/PLT DEMO/W2_PLT_DEMO/Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCharacter : MonoBehaviour {
+
+	public CharacterController playerControl;
+	// Use this for initialization
+	public float maxSpeed = 1f;
+	public Vector3 speedVector;
+	void Start () {
+		speedVector = new Vector3 (0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+		gameObject.GetComponent<Renderer>().material.color = Color.magenta;
+		
+
+		float playerInputX = Input.GetAxis("Horizontal");
+		float playerInputZ = Input.GetAxis("Vertical");
+
+		speedVector.x = playerInputX*maxSpeed;
+		speedVector.z = playerInputZ*maxSpeed;
+
+		Vector3 pos = transform.position;
+
+		pos += speedVector;
+
+		transform.position = pos;
+
+		//playerControl.SimpleMove (speedVector);
+	}
+}
